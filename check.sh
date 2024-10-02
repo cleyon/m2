4bf4c20571dcebe163db415a7dfba3f4c588fcdf
Chris Leyon <cleyon@gmail.com>
2024-10-02 17:30:16 -0400

    Tweaks to check.sh - add # tests, pass/fail

--- check.sh
+++ check.sh
@@ -4,8 +4,8 @@
 # Copyright (C) 2006-2023 Antonio Diaz Diaz.
 # This script is free software; you have unlimited permission
 # to copy, distribute, and modify it.
-
-
+#
+#
 # CATEGORY/SERIES/TESTNAME              (CATEGORY/SERIES is called test_id)
 # ========================
 # Run the *.m2 "scripts" and compare their output against the TESTNAME.out files,
@@ -14,8 +14,8 @@
 # It is expected that m2 will succeed and exit with a zero status.  If
 # that is not the case (i.e., you expect the test to fail and exit with
 # a non-zero status), the expected code should be put in TESTNAME.exit
-
-
+#
+#
 # FILE NAMING CONVENTION
 # ======================
 # User-specified config files:
@@ -36,20 +36,26 @@
 # TESTNAME.expected_err     Copy of desired error text, if any, default blank
 # TESTNAME.expected_exit    Copy of desired exit code
 # TESTNAME.expected_out     Copy of desired output text, if any, default blank
+#
+#
+# EXIT STATUS
+# ===========
+# 0     all tests passed
+# 127   at least one test failed
 
 
-debug="false"		# "true"
-busybox_awk="false"	# "true"
+# These two variables are user settable:
+debug="false"		# set to "true" for extra messages
+busybox_awk="false"	# set to "true" to invoke m2 as "busybox awk -f ..."
+
 
 LC_ALL=C
 export LC_ALL
-objdir=`pwd`
-[ $debug = "true" ] && echo "objdir  is $objdir"
-# test dir used to be parameter #1, but no longer
-#testdir=`cd "$1" ; pwd`
-testdir="`pwd`/tests"
-[ $debug = "true" ] && echo "testdir is $testdir"
-new_M2="${objdir}"/m2
+rc=0
+ntest=0
+npass=0
+nfail=0
+
 
 framework_error()
 {
@@ -61,18 +67,31 @@ framework_error()
     exit 1
 }
 
-if [ ! $busybox_awk = "true" ]; then
+
+summarize_tests()
+{
+    local pctpass=0
+    local pctfail=0
+    [ $npass -ne 0 ] && pctpass=`expr \( $ntest \* 100 \) / $npass`
+    [ $nfail -ne 0 ] && pctfail=`expr \( $ntest \* 100 \) / $nfail`
+    printf "!!! SUMMARY - %d total tests: %d passed (%.0f%%), %d failed (%.0f%%)\n" \
+           $ntest $npass $pctpass $nfail $pctfail
+}
+
+
+objdir=`pwd`
+[ $debug = "true" ] && echo "objdir  is $objdir"
+new_M2="${objdir}"/m2
+if [ $busybox_awk = "true" ]; then
+    busybox awk -f $new_M2 /dev/null >/dev/null 2>&1
+    [ $? -eq 0 ] || framework_error "Error executing \"busybox awk -f $new_M2\""
+else
     [ -f "${new_M2}" -a -x "${new_M2}" ] || framework_error "${new_M2}: Cannot execute"
 fi
 
-# cat "${testdir}"/test.txt > test.txt || framework_error
-fail=0
-
-if [ -d tmp ] ; then rm -rf tmp ; fi
-mkdir tmp
-cd "${objdir}"/tmp || framework_error "Could not change directory to ${objdir}/tmp"
-tmpdir=`pwd`
-[ $debug = "true" ] && echo "tmpdir  is $tmpdir"
+testdir="`pwd`/tests"
+[ -d $testdir ] || framework_error "$testdir is not a directory"
+[ $debug = "true" ] && echo "testdir is $testdir"
 
 
 test_all_categories()
@@ -89,6 +108,7 @@ test_category()
 {
     local CATEGORY=$1
 
+    [ -d $CATEGORY ] || framework_error "$CATEGORY is not a directory"
     cd "$CATEGORY"
     for SERIES in ???; do
         [ "$SERIES" = "???" ] && continue # if no numbered subdirectories
@@ -114,7 +134,7 @@ test_series()
     for M2_FILE in *.m2 ; do
         [ $debug = "true" ] && echo "cwd is `pwd`"
         [ $debug = "true" ] && echo "path is \"$CATEGORY/$SERIES/$M2_FILE\""
-        run_test "${CATEGORY}" "${SERIES}" "${M2_FILE}" "${test_id}"
+        run_test "${CATEGORY}" "${SERIES}" "${M2_FILE}"
     done
     cd ..
 }
@@ -125,6 +145,7 @@ run_test()
     local CATEGORY=$1
     local SERIES=$2
     local M2_FILE=$3
+
     local test_id=`echo $CATEGORY/$SERIES | sed "s,${testdir}/,,"`
     local TESTNAME
 
@@ -140,13 +161,14 @@ run_test()
 
     [ -f ${TESTNAME}.disabled ] && { echo "SKIP - Test disabled"; return; }
     [ -s "$M2_FILE" ] || { echo "SKIP - Empty test file"; return; }
-    [ -r "$M2_FILE" ] || { echo "FAIL - Unreadable test file"; fail=127; return; }
-    [ -f ${TESTNAME}.out ] || { echo "FAIL - ${TESTNAME}.out does not exist!"; fail=127; return; }
+    [ -r "$M2_FILE" ] || { echo "FAIL - Unreadable test file"; rc=127; return; }
+    [ -f ${TESTNAME}.out ] || { echo "FAIL - ${TESTNAME}.out does not exist!"; rc=127; return; }
 
     rm -f ${TESTNAME}.expected_out ${TESTNAME}.expected_err ${TESTNAME}.expected_exit
     rm -f ${TESTNAME}.run_out      ${TESTNAME}.run_err      ${TESTNAME}.run_exit
-    trap 'rm -f ${TESTNAME}.expected_out ${TESTNAME}.expected_err ${TESTNAME}.expected_exit ${TESTNAME}.run_out ${TESTNAME}.run_err ${TESTNAME}.run_exit; exit' \
+    trap 'rm -f ${TESTNAME}.expected_out ${TESTNAME}.expected_err ${TESTNAME}.expected_exit ${TESTNAME}.run_out ${TESTNAME}.run_err ${TESTNAME}.run_exit; summarize_tests; exit' \
          1 2 3 15
+    ntest=$(expr $ntest + 1)
 
     cp ${TESTNAME}.out ${TESTNAME}.expected_out
     if [ -f ${TESTNAME}.exit ]; then
@@ -170,11 +192,13 @@ run_test()
     if ! cmp -s ${TESTNAME}.run_exit ${TESTNAME}.expected_exit; then
         echo "FAIL - Unexpected exit code"
         echo "    Exit code `cat ${TESTNAME}.run_exit`;  wanted `cat ${TESTNAME}.expected_exit`"
-        fail=127
+        nfail=$(expr $nfail + 1)
+        rc=127
     fi
     if ! cmp -s ${TESTNAME}.run_out ${TESTNAME}.expected_out; then
         echo "FAIL - Unexpected output"
         echo "    (file $CATEGORY/$SERIES/$M2_FILE)"
+        nfail=$(expr $nfail + 1)
         if [ -f ${TESTNAME}.showdiff ]; then
             echo ">>> DIFF EXPECTED/ACTUAL OUTPUT TEXT <<<"
             echo diff -c ${TESTNAME}.expected_out ${TESTNAME}.run_out
@@ -189,17 +213,19 @@ run_test()
             echo ">>> ERRORS <<<"
             cat ${TESTNAME}.run_err
         fi
-        fail=127
+        rc=127
     elif ! cmp -s ${TESTNAME}.run_err ${TESTNAME}.expected_err; then
         echo "FAIL - Unexpected error messages"
         echo "    (file $CATEGORY/$SERIES/$M2_FILE)"
+        nfail=$(expr $nfail + 1)
         echo ">>> EXPECTED ERRORS <<<"
         cat ${TESTNAME}.expected_err
         echo ">>> ACTUAL ERRORS <<<"
         cat ${TESTNAME}.run_err
-        fail=127
+        rc=127
     else
         echo "PASS"
+        npass=$(expr $npass + 1)
         rm -f ${TESTNAME}.run_out ${TESTNAME}.run_err
     fi
 
@@ -253,11 +279,12 @@ case $# in
     *) framework_error "Invocation error: Bad # parameters" ;;
 esac
 
-#rm -f test.txt
-if [ ${fail} -eq 0 ] ; then
+
+if [ ${rc} -eq 0 ] ; then
     echo "!!! SUCCESS - Tests completed successfully"
-    cd "${objdir}" && rm -r tmp
 else
     echo "!!! FAILURE - Some tests failed"
 fi
-exit ${fail}
+
+summarize_tests
+exit ${rc}
